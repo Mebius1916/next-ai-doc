0f441044bf9bf514e69eb4f4dabbbbdf24c339e7
mebius <591626940@qq.com>
2025-02-17 03:09:31 +0800

    注释

--- src/hooks/use-debounce.ts
+++ src/hooks/use-debounce.ts
@@ -1,17 +1,37 @@
-import {useCallback, useRef} from "react";
+import { useCallback, useRef } from "react";
 
 export function useDebounce<
   T extends (...args: Parameters<T>) => ReturnType<T>,
 >(callback: T, delay: number = 500) {
   const timeoutRef = useRef<NodeJS.Timeout>();
 
-  return useCallback((...args: Parameters<T>) => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-    
-    timeoutRef.current = setTimeout(() => {
-      callback(...args);
-    }, delay);
-  }, [callback, delay]);
+  return useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+      //存储的setTimeout 返回的定时器ID
+      timeoutRef.current = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    },
+    [callback, delay]
+  );
 }
+
+//js版本：
+// import { useCallback, useRef } from "react";
+// export function useDebounce(callback, delay = 500) {
+//   const timeoutRef = useRef();
+//   return useCallback(
+//     (...args) => {
+//       if (timeoutRef.current) {
+//         clearTimeout(timeoutRef.current);
+//       }
+//       timeoutRef.current = setTimeout(() => {
+//         callback(...args);
+//       }, delay);
+//     },
+//     [callback, delay]
+//   );
+// }
